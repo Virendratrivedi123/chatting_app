8d61a2eb95d613e08429bb1de574c745ddfbf80c
Virendratrivedi123 <116142393+Virendratrivedi123@users.noreply.github.com>
2022-11-02 16:38:49 +0530

    ac

---
+++ lib/screens/otp.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Myhome extends StatefulWidget {
+  const Myhome({super.key});
+
+  @override
+  State<Myhome> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<Myhome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
